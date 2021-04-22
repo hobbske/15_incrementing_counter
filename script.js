91bc2a4f51a12f52775e81423c9f0ba493c5669b
Kevin Hobbs <kevin21921@gmail.com>
2021-04-21 23:45:22 -0400

    Added methods to js for counter

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+  counter.innerText = '0'
+  
+  const updateCounter = () => {
+   
+    const target = +counter.getAttribute('data-target') 
+    const increment = target / 200
+    const c = +counter.innerText
+    
+
+  
+    
+
+
+    if(c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 1)
+    }else {
+      counter.innerText = target
+    }
+  }
+ 
+  updateCounter()
+})
+
 
